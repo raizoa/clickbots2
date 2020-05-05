5ccfe50482a2b7de0cc1d560412ee6481d394ad0
raizoa <64818420+raizoa@users.noreply.github.com>
2020-05-05 09:53:32 +0700

    Add files via upload

---
+++ bot1.sh
@@ -0,0 +1,22 @@
+     #10 akun tele ltc
+     python main.py +6283112007689 ltc &&
+     sleep 2
+     python main.py +6283112007687 ltc &&
+     sleep 2
+     python main.py +19282224934 ltc &&
+     sleep 2
+     python main.py +16612286536 ltc &&
+     sleep 2
+     python main.py +18312025795 ltc &&
+     sleep 2
+     python main.py +16614270166 ltc &&
+     sleep 2
+     python main.py +16615275804 ltc &&
+     sleep 2
+     python main.py +16612805617 ltc &&
+     sleep 2
+     python main.py +16612320717 ltc &&
+     sleep 2
+     python main.py +16619276763 ltc &&
+     sleep 2
+     sh bot1.sh
